ed035d9aae9d49dd3909cad6b2b24d7d73c36c59
Alex Lee <alex.lee@healthsqyre.com>
2020-07-05 18:35:49 -0400

    add contact

--- resources/js/index.js
+++ resources/js/index.js
@@ -2,53 +2,60 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function typeWriter() {
+async function greetingTypeWriterReturn() {
+    document.getElementById('greeting-text-2').innerHTML = "Hi, I'm Alex Lee!";
+    await sleep(3000);
+    document.getElementById('greeting-text-2').innerHTML = "<span>&#8203;</span>";
+}
+
+async function typeWriter(elemId, text) {
     var i = 0;
-    var greetingText = "> print(\"Hi, I'm Alex Lee!\")";
-    var greetingTextLength = greetingText.length;
-    var j = greetingTextLength;
+    var textLength = text.length;
+    var j = textLength;
     while (true) {
-        if (i < greetingText.length) {
-            document.getElementById('greeting-text').innerHTML += greetingText.charAt(i);
+        if (i < textLength) {
+            document.getElementById(elemId).innerHTML += text.charAt(i);
             i++;
             await sleep(75);
-            if (i == greetingTextLength) {
-                document.getElementById('greeting-text-2').innerHTML = "Hi, I'm Alex Lee!";
-                await sleep(3000);
-                document.getElementById('greeting-text-2').innerHTML = "<span>&#8203;</span>";
+            if (i == textLength) {
+                if (elemId == 'greeting-text') {
+                    await greetingTypeWriterReturn();
+                } else if (elemId == 'contact-text') {
+                    await sleep(3000);
+                }
             }
-        } else if (i == greetingTextLength && j > 2) {
-            document.getElementById('greeting-text').innerHTML = greetingText.slice(0, j - 1);
+        } else if (i == textLength && j > 2) {
+            document.getElementById(elemId).innerHTML = text.slice(0, j - 1);
             j--;
             await sleep(60);
         } else {
             i = 1;
-            j = greetingTextLength;
+            j = textLength;
         }
     }
 }
 
-function activeTabScroll() {
-    window.addEventListener('scroll', function() {
-        var navLinks = document.getElementsByClassName("nav-link");
-        for (var i = 0; i < navLinks.length; i++) {
-            var sectionId = navLinks[i].href.split('#').pop();
-            var sectionElem = document.getElementById(sectionId);
-            var sectionElemRect = sectionElem.getBoundingClientRect();
-            var sectionElemBottom = Math.floor(sectionElemRect.bottom);
-            var sectionElemHeight = Math.floor(sectionElemRect.height);
-            if (sectionElemBottom <= sectionElemHeight && sectionElemBottom > 0) {
-                if (!navLinks[i].classList.contains('active')) {
-                    navLinks[i].classList.add('active');
-                }
-            } else {
-                if (navLinks[i].classList.contains('active')) {
-                    navLinks[i].classList.remove('active');
-                }
+function activeTab() {
+    var navLinks = document.getElementsByClassName("nav-link");
+    for (var i = 0; i < navLinks.length; i++) {
+        var sectionId = navLinks[i].href.split('#').pop();
+        var sectionElem = document.getElementById(sectionId);
+        var sectionElemRect = sectionElem.getBoundingClientRect();
+        var sectionElemBottom = Math.floor(sectionElemRect.bottom);
+        var sectionElemHeight = Math.floor(sectionElemRect.height);
+        if (sectionElemBottom <= sectionElemHeight && sectionElemBottom > 0) {
+            if (!navLinks[i].classList.contains('active')) {
+                navLinks[i].classList.add('active');
+            }
+        } else {
+            if (navLinks[i].classList.contains('active')) {
+                navLinks[i].classList.remove('active');
             }
         }
-    });
+    }
 }
 
-activeTabScroll();
-typeWriter();
+window.addEventListener("load", activeTab);
+window.addEventListener("scroll", activeTab);
+typeWriter('greeting-text', "> console.log(\"Hi, I'm Alex Lee!\");")
+typeWriter('contact-text', "> if (youLikeWhatYouSee) letsGetInTouch();");
